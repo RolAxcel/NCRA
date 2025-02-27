2d6ce2b4866fb8a9dec52ebbdd167905ba6f2fde
RolAxcel <155981523+RolAxcel@users.noreply.github.com>
2025-02-27 16:17:27 +0800

    Add files via upload

---
+++ assesment_record/resize.js
@@ -0,0 +1,8 @@
+const textarea = document.getElementById('textarea');
+
+textarea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+  this.style.height = 'auto';
+  this.style.height = this.scrollHeight + 'px';
+}
